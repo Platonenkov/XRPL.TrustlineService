2b249dae780262bdbbdffca9a0997562f222acb6
Aleksandr <platonenkov87@gmail.com>
2022-12-03 11:54:40 +0300

    fix nft points, removed point for all nft

--- TrustlineService/XrplTrustlineClient.cs
+++ TrustlineService/XrplTrustlineClient.cs
@@ -1,13 +1,13 @@
-﻿using Newtonsoft.Json;
-
-using XRPL.TrustlineService.Domain;
+﻿using XRPL.TrustlineService.Domain;
 using XRPL.TrustlineService.Domain.Xls20;
 
 namespace XRPL.TrustlineService
 {
+
     /// <summary> client to connect to <a>https://api.xrpldata.com/</a></summary>
     public class XrplTrustlineClient : BaseClient
     {
+        public int ApiVersion { get; set; } = 1;
         public XrplTrustlineClient():base("https://api.xrpldata.com/")
         {
         }
@@ -15,19 +15,19 @@ namespace XRPL.TrustlineService
         /// <returns><seealso cref="TrustlinesServerResponse"/></returns>
         public async Task<TrustlinesServerResponse> GetKnownTrustlines(CancellationToken Cancel = default)
         {
-            var response = await GetAsync<TrustlinesServerResponse>("api/v1/tokens", Cancel);
+            var response = await GetAsync<TrustlinesServerResponse>("api/v{ApiVersion}/tokens", Cancel);
             return response;
         }
-        /// <summary> get all XLS20 NFT info </summary>
-        public async Task<BaseServerResponse<List<IssuerXls20Nft>>> GetXls20NFT(CancellationToken Cancel = default)
-        {
-            var response = await GetAsync<BaseServerResponse<List<IssuerXls20Nft>>>("api/v1/xls20-nfts", Cancel);
-            return response;
-        }
+        ///// <summary> get all XLS20 NFT info </summary>
+        //public async Task<BaseServerResponse<List<IssuerXls20Nft>>> GetXls20NFT(CancellationToken Cancel = default)
+        //{
+        //    var response = await GetAsync<BaseServerResponse<List<IssuerXls20Nft>>>("api/v{ApiVersion}/xls20-nfts", Cancel);
+        //    return response;
+        //}
         /// <summary> Returns an array of XRPL accounts which have currently issued NFTs on the XRP Ledger. </summary>
         public async Task<BaseServerResponse<Xls20NftIssuers>> GetAllNFTIssuer(CancellationToken Cancel = default)
         {
-            var response = await GetAsync<BaseServerResponse<Xls20NftIssuers>>($"api/v1/xls20-nfts/all/issuers", Cancel);
+            var response = await GetAsync<BaseServerResponse<Xls20NftIssuers>>($"api/v{ApiVersion}/xls20-nfts/all/issuers", Cancel);
 
             return response;
         }
@@ -39,20 +39,20 @@ namespace XRPL.TrustlineService
             {
                 address += $"/taxon/{Taxon}";
             }
-            var response = await GetAsync<BaseServerResponse<IssuerXls20Nft>>($"api/v1/xls20-nfts/issuer/{address}", Cancel);
+            var response = await GetAsync<BaseServerResponse<IssuerXls20Nft>>($"api/v{ApiVersion}/xls20-nfts/issuer/{address}", Cancel);
             return response;
         }
         /// <summary> get all Issuer taxon (collections) </summary>
         public async Task<BaseServerResponse<Xls20TaxonResponse>> GetIssuerTaxon(string Issuer, CancellationToken Cancel = default)
         {
-            var response = await GetAsync<BaseServerResponse<Xls20TaxonResponse>>($"api/v1/xls20-nfts/taxon/{Issuer}", Cancel);
+            var response = await GetAsync<BaseServerResponse<Xls20TaxonResponse>>($"api/v{ApiVersion}/xls20-nfts/taxon/{Issuer}", Cancel);
 
             return response;
         }
         /// <summary> get NFT info </summary>
         public async Task<BaseServerResponse<Xls20NftResponse>> GetNftInfoById(string NftId, CancellationToken Cancel = default)
         {
-            var response = await GetAsync<BaseServerResponse<Xls20NftResponse>>($"api/v1/xls20-nfts/nft/{NftId}", Cancel);
+            var response = await GetAsync<BaseServerResponse<Xls20NftResponse>>($"api/v{ApiVersion}/xls20-nfts/nft/{NftId}", Cancel);
             return response;
         }
     }
