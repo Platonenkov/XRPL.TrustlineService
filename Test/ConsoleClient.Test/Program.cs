9bbe012364c1040e09c56211a8fe89ebcef20a9f
Aleksandr <platonenkov87@gmail.com>
2022-12-14 20:09:56 +0300

    nfts offers info

--- Test/ConsoleClient.Test/Program.cs
+++ Test/ConsoleClient.Test/Program.cs
@@ -5,55 +5,17 @@ using XRPL.TrustlineService;
 
 
 XrplTrustlineClient trust = new XrplTrustlineClient(); //create client
-//var lines = await trust.GetKnownTrustlines();  //download tustlines
-//if (lines is null)
-//{
-//    Console.WriteLine("Database not found");
-//    return;
-//}
-//var trustlines = lines.Map();  // Map to IEnumerable
-
-//var count = trustlines.ToArray().Length;
-//Console.WriteLine($"{count} Trustlines in database");
-//Console.WriteLine();
-//var RLT_Currency = trustlines.FirstOrDefault(f => f.CurrencyCode == "RLT");
-//Console.WriteLine(
-//    RLT_Currency != null
-//        ? JObject.Parse(JsonConvert.SerializeObject(RLT_Currency))
-//        : JObject.Parse(JsonConvert.SerializeObject(trustlines?.FirstOrDefault()))
-//        );
-//Console.WriteLine();
-
-//var nfts = await trust.GetXls20NFT();   //download nfts database
-//var nfts_issuers = nfts.nfts.Count;
-//Console.WriteLine($"{nfts_issuers} NFT issuers");
-//Console.WriteLine();
-
 var AllNFTIssuer = await trust.GetAllNFTIssuer();  //all nfts issuers
 var IssuerNFT = await trust.GetIssuerNFT("rpqqyZXhowA6kb3eQKqRGA3GxbjzZFfMAD");  //download issuer nfts
 var AccountNFT = await trust.GetAccountNFTs("rLiooJRSKeiNfRJcDBUhu4rcjQjGLWqa4p");  //download account nfts
-//var Xls20NFT = await trust.GetXls20NFT();  //download issuer taxon
 var IssuerTaxon = await trust.GetIssuerTaxon("rpqqyZXhowA6kb3eQKqRGA3GxbjzZFfMAD");  //download issuer taxon
 var NftInfoById = await trust.GetNftInfoById("000827106CD2CBB743BE141A0FE7EA1F3177161ED3CCDCB21EE07C59000025BD");  //NFT info by ID
 var KnownTrustlines = await trust.GetKnownTrustlines();  //download trustlines
 var lines = KnownTrustlines.issuers.SelectMany(c => c.Value.tokens).ToArray();
+
+var issuer_nfts_offers = await trust.GetIssuerNFTsOffers("rBZ3FrRzTgy46H3C9Uede8CAuH7rTGA5iE");
+var issuer_nfts_offers_with_taxon = await trust.GetIssuerNFTsOffers("rBZ3FrRzTgy46H3C9Uede8CAuH7rTGA5iE", 0);
+var account_nfts_offers = await trust.GetAccountNFTsOffers("rLiooJRSKeiNfRJcDBUhu4rcjQjGLWqa4p");
+var nft_offers = await trust.GetNFTokenIDOffers("000827106CD2CBB743BE141A0FE7EA1F3177161ED3CCDCB21EE07C59000025BD");
+var nft_offer_info = await trust.GetNFTOfferInfo("2AA3FD4A61C4F968E40B1B49DB61281BD8CDE3674E8B8FECF255ECF6C9F64719");
 Console.WriteLine();
-//Console.WriteLine($"{IssuerTaxon.Data.Issuer} has {IssuerTaxon.Data.Taxon.Count} collections");
-//var collections = (await trust.GetIssuerNFT("rpqqyZXhowA6kb3eQKqRGA3GxbjzZFfMAD")).Data.NFTs.GroupBy(c=>c.Taxon).ToArray();
-//foreach (var t in IssuerTaxon.Data.Taxon)
-//{
-//    var issuer_nfts = await trust.GetIssuerNFT("rpqqyZXhowA6kb3eQKqRGA3GxbjzZFfMAD", t);  //download issuer nfts
-//    Console.WriteLine($"{t} collection: count = {issuer_nfts.Data.NFTs.Count}");
-//}
-
-//var issuer_nfts1 = await trust.GetIssuerNFT("rwvQWhjpUncjEbhsD2V9tv4YpKXjfH5RDj");  //download issuer nfts
-//Console.WriteLine($"{issuer_nfts1.Data.Issuer} has {issuer_nfts1.Data.NFTs.Count} NFT");
-//Console.WriteLine();
-
-//var nft_info = await trust.GetNftInfoById("000827106CD2CBB743BE141A0FE7EA1F3177161ED3CCDCB21EE07C59000025BD");  //download nft info
-//Console.WriteLine(
-//    nft_info != null
-//        ? JObject.Parse(JsonConvert.SerializeObject(nft_info))
-//        : null);
-
-//Console.WriteLine("Hello, from XrplDaddy!");
---
+++ TrustlineService/Domain/NFTsOffers/NFTOfferInfo.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace XRPL.TrustlineService.Domain.NFTsOffers;
+
+public class NFTOfferInfo
+{
+    [JsonProperty("offerid")]
+    public string OfferID { get; set; }
+    [JsonProperty("offer")]
+    public NFTOffer Offer { get; set; }
+}
